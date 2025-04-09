ad4716f06d1ff2d298fad75404f87b5d36c42e37
Lewie-Mavis <55090466+Lewie-Mavis@users.noreply.github.com>
2025-04-09 15:35:24 +0300

    Add files via upload

---
+++ portfolio_hellen/scripts/slider.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+    var slider = $('.gallery-slider');
+
+    slider.slick({
+        slidesToShow: 3,        // Show 3 images at a time
+        slidesToScroll: 1,      // Scroll 1 image at a time
+        autoplay: true,         // Enable auto sliding
+        autoplaySpeed: 2000,    // Slide change speed in ms
+        dots: true,             // Show navigation dots
+        arrows: true,           // Show navigation arrows
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2   // Show 2 images on medium screens
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1   // Show 1 image on small screens
+                }
+            }
+        ]
+    });
+
+    // Ensure autoplay continues after clicking on an image
+    $('.gallery-slider img').on('click', function() {
+        slider.slick('slickPlay');  // Resume autoplay after clicking an image
+    });
+});
