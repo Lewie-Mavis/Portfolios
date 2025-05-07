bb62627e8c2cf571873427e1e94480c081f0b985
Lewie-Mavis <lewielom@gmail.com>
2025-05-07 11:20:08 +0300

    Add via file upload

---
+++ portfolio_wale_akinyemi/scripts/swiper.js
@@ -0,0 +1,23 @@
+  document.addEventListener('DOMContentLoaded', function () {
+    const swiper = new Swiper('.events-slider', {
+      loop: true,
+      slidesPerView: 1,
+      spaceBetween: 30,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+      },
+      breakpoints: {
+        768: {
+          slidesPerView: 2
+        },
+        1024: {
+          slidesPerView: 3
+        }
+      }
+    });
+  });
